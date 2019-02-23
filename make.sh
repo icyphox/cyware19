baf3048e2544ba89d4e532c4e56355eedef187a3
Anirudh <icyph0x@pm.me>
2019-02-23 16:00:13 +0530

    Add hello.c, redirect output to /dev/null
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+gcc src/hello.c -o hello -no-pie
 gcc src/crackme02.c -o crackme02 -m32 -no-pie -fno-stack-protector
 gcc src/crackme01.c -o crackme01 -m32 -no-pie -fno-stack-protector
-gcc src/crackme03.c -o crackme03 -m32 -no-pie -fno-stack-protector
+gcc src/crackme03.c -o crackme03 -m32 -no-pie -fno-stack-protector > /dev/null 2>&1
 echo "all done"
